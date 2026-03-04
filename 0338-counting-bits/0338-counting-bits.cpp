44dadab6102c9b749ccd791ef9e7c5baf4f63493
Yuvraj Dholia <100825799+yuv03@users.noreply.github.com>
2026-03-04 23:05:55 +0530

    Time: 0 ms (100%), Space: 10.7 MB (58.35%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,19 +1,33 @@
+// class Solution {
+// public:
+//     vector<int> countBits(int n) {
+//         vector<int> count(1,0);
+//         if(n==0) return count;
+//         for(int i=1; i<n+1; i++){ 
+//             int rem = 0;
+//             int quo = i;
+//             int temp =0;
+//             while(quo){
+//                 rem = quo%2;
+//                 quo = quo/2;
+//                 if(rem==1) temp++;
+//             }
+//             count.push_back(temp);
+//         }
+//         return count;
+//     }
+// };
+
+
+// Note: With the use of AND operation here we are saving some time and here the AND operation is removing the righmost set bit of number i so we do +1
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> count(1,0);
-        if(n==0) return count;
-        for(int i=1; i<n+1; i++){ 
-            int rem = 0;
-            int quo = i;
-            int temp =0;
-            while(quo){
-                rem = quo%2;
-                quo = quo/2;
-                if(rem==1) temp++;
-            }
-            count.push_back(temp);
+        vector<int> count(n+1, 0);
+        for(int i=1; i<n+1; i++){
+            count[i] = count[i&(i-1)]+1;
         }
         return count;
     }
 };
+
