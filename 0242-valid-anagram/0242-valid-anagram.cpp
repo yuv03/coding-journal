46d93e0680c57770edc86c630b6a4b1ced12f79e
Yuvraj Dholia <100825799+yuv03@users.noreply.github.com>
2026-03-24 16:05:52 +0530

    Time: 4 ms (35.81%), Space: 9.7 MB (73.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,26 +1,60 @@
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if (s.size() != t.size())
+//             return false;
+
+//         vector<int> svec(26, 0);
+//         vector<int> tvec(26, 0);
+
+//         for (int i = 0; i < s.size(); i++) {
+//             svec[s[i] - 'a']++;
+//         }
+
+//         for (int i = 0; i < t.size(); i++) {
+//             tvec[t[i] - 'a']++;
+//         }
+
+//         int i = 0;
+//         while (i < 26) {
+//             if (svec[i] != tvec[i])
+//                 return false;
+//             i++;
+//         }
+//         return true;
+//     }
+// };
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size() != t.size())
-            return false;
-
-        vector<int> svec(26, 0);
-        vector<int> tvec(26, 0);
-
-        for (int i = 0; i < s.size(); i++) {
-            svec[s[i] - 'a']++;
+        if(s.size() != t.size()) return false;
+        map<char, int> ms;
+        map<char, int> mt;
+        for(int i=0; i<s.size(); i++){
+            ms[s[i]]++;
+            mt[t[i]]++;
         }
 
-        for (int i = 0; i < t.size(); i++) {
-            tvec[t[i] - 'a']++;
-        }
+        map<char, int>::iterator its = ms.begin();
+        map<char, int>::iterator itt = mt.begin();
 
-        int i = 0;
-        while (i < 26) {
-            if (svec[i] != tvec[i])
-                return false;
-            i++;
+        while(its != ms.end() && itt != mt.end()){
+            if(its->first == itt->first){
+                if(its->second != itt->second) return false;
+            }
+            else return false;
+            its++;
+            itt++;
         }
+        if(its != ms.end()) return false;
+        if(itt != mt.end()) return false;
         return true;
+
     }
 };
+
+
+
+
+
