2f2cf1d5bc7782d23fbeab58211bd1b67a6c8a98
Yuvraj Dholia <100825799+yuv03@users.noreply.github.com>
2026-04-10 23:53:30 +0530

    Time: 0 ms (100%), Space: 8.3 MB (10.69%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
+    int f(int n, vector<int>& dp){
+        if(dp[n]!=-1) return dp[n];
+        if(n<=1) {
+            dp[n] = n;
+            return n;
+        }
+        dp[n] = f(n-1, dp) + f(n-2, dp);
+        return dp[n];
+    }
+
     int fib(int n) {
-        if(n<=0) return 0;
-        if(n==1) return 1;
-        return fib(n-1) + fib(n-2);
+        vector<int> dp(n+1,-1);
+        return f(n, dp);
+        
     }
 };
