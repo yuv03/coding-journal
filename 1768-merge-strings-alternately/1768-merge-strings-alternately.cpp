3aaa452c764b5ec067bc09bffa20921af1ac2dae
Yuvraj Dholia <100825799+yuv03@users.noreply.github.com>
2026-04-30 22:17:06 +0530

    Time: 0 ms (100%), Space: 8.4 MB (63.85%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string str;
+        int i=0;
+        int j=0;
+        while(i<word1.size() && j<word2.size()){
+            str.push_back(word1[i]);
+            str.push_back(word2[j]);
+            i++;
+            j++;
+        }
+        while(i<word1.size()){
+            str.push_back(word1[i]);
+            i++;
+        }
+        while(j<word2.size()){
+            str.push_back(word2[j]);
+            j++;
+        }
+        return str;
+    }
+};
