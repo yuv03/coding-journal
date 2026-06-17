2b4d88d85de388aa23957645a14b52cb25e29fda
Yuvraj Dholia <100825799+yuv03@users.noreply.github.com>
2026-06-17 20:29:15 +0530

    Time: 29 ms (41.67%), Space: 17.5 MB (42.92%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,46 +1,37 @@
 class Solution {
 private:
-    void dfs(int n, int m, int row, int col, vector<vector<char>> &grid, vector<vector<int>> &vis){
-        vis[row][col]=1;
-        int rowFor = row+1;
-        int rowBack = row-1;
-        int colFor = col +1;
-        int colBack = col -1;
+    void dfs(int n, int m, int row, int col, vector<vector<char>>& grid,
+             vector<vector<int>>& vis) {
+        vis[row][col] = 1;
 
-        if(col>=0 && col<m && rowBack >=0 && rowBack < n && grid[rowBack][col]=='1' && vis[rowBack][col]==0){
-            dfs(n,m,rowBack, col, grid, vis);
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, 1, 0, -1};
+
+        for (int i = 0; i < 4; i++) {
+            int r = row + delRow[i];
+            int c = col + delCol[i];
+
+            if (r >= 0 && r < n && c >= 0 && c < m && !vis[r][c] &&
+                grid[r][c] == '1')
+                dfs(n, m, r, c, grid, vis);
         }
-
-        if(col>=0 && col<m && rowFor<n && rowFor >=0 && grid[rowFor][col]=='1' && vis[rowFor][col]==0){
-            dfs(n,m,rowFor, col, grid, vis);
-        }
-
-        if(row>=0 && row<n &&colBack >=0 && colBack<m && grid[row][colBack]=='1' && vis[row][colBack]==0){
-            dfs(n,m,row, colBack, grid, vis);
-        }
-
-        if(row>=0 && row<n && colFor >=0 && colFor<m && grid[row][colFor]=='1' && vis[row][colFor]==0){
-            dfs(n,m,row, colFor, grid, vis);
-        }
-
-        
     }
+
 public:
     int numIslands(vector<vector<char>>& grid) {
+        int isl = 0;
         int n = grid.size();
         int m = grid[0].size();
-        int count = 0;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
 
-        vector<vector<int>> vis(n,vector<int>(m,0));
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(grid[i][j]=='1' && vis[i][j]==0){
-                    count++;
-                    dfs(n,m,i, j, grid, vis);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!vis[i][j] && grid[i][j] == '1') {
+                    isl++;
+                    dfs(n, m, i, j, grid, vis);
                 }
             }
         }
-        return count;
+        return isl;
     }
 };
