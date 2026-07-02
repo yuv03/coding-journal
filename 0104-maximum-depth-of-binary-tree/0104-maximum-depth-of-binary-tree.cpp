78b7a585c2ce04f69c0a2ce857109e636943a4ce
Yuvraj Dholia <100825799+yuv03@users.noreply.github.com>
2026-07-02 22:38:59 +0530

    Time: 0 ms (100%), Space: 22.2 MB (14.16%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -6,21 +6,42 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
+
+private:
+    void solve(TreeNode* root, int &depth, int tempHeight){
+        if(root == NULL) return;
+        tempHeight++;
+        depth = max(depth, tempHeight);
+
+        solve(root->left, depth, tempHeight);
+        solve(root->right, depth, tempHeight);
+    }
 public:
     int maxDepth(TreeNode* root) {
-
-        if(root ==NULL){
-            return 0;
-        }
-
-        int left = maxDepth(root->left);
-        int right = maxDepth(root->right);
-        return max(left, right) + 1;
-
-        
+        if(!root) return 0;
+        int depth = 0;
+        solve(root, depth, 0);
+        return depth;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
