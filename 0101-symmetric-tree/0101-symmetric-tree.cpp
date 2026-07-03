a22d014782f25f82009c7049eed78c68979995e7
Yuvraj Dholia <100825799+yuv03@users.noreply.github.com>
2026-07-03 23:00:43 +0530

    Time: 0 ms (100%), Space: 18.5 MB (27.98%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -1,34 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
-
-private:
-    bool solve(TreeNode* l, TreeNode* r) {
-        if(l==NULL && r==NULL) return true;
-        else if(l==NULL && r != NULL) return false;
-        else if(r==NULL && l != NULL) return false;
-        else if(l != NULL && r != NULL && l->val != r->val) return false;
-
-        bool a = solve(l->left, r->right);
-        bool b = solve(l->right, r->left);
-
-        if(a && b) return true;
-        else return false;
-    }
-
 public:
+    
+    void solve(TreeNode* l, TreeNode* r, bool& flag){
+        
+        if(l==NULL && r==NULL ) return ;
+        if(l==NULL && r!=NULL){
+            flag = false;
+            return ;
+        }
+        else if(l!=NULL && r==NULL){
+            flag = false;
+            return ;
+        }
+    
+        if(l->val != r->val) flag = false;
+        
+        solve(l->left, r->right, flag);
+        solve(l->right, r->left, flag);        
+    }
+    
     bool isSymmetric(TreeNode* root) {
+        bool flag = true;
         if(root == NULL) return true;
-        return solve(root->left, root->right);
+        if(root->left == NULL && root->right == NULL) return true;
+        solve(root->left, root->right, flag);
+        return flag;
+        
     }
 };
