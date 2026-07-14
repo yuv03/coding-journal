d61c50429b36d2e8509559e44d8af5ee7ed8bda2
Yuvraj Dholia <100825799+yuv03@users.noreply.github.com>
2026-07-14 22:20:27 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char, int> ms;
+        map<char, int> mt;
+
+        if(s.size() != t.size()) return false;
+
+        for(int i=0; i<s.size(); i++){
+            ms[s[i]]++;
+            mt[t[i]]++;
+        }
+
+        if(ms.size() != mt.size()) return false;
+
+        for(auto it : ms){
+            char ch = it.first;
+            int freq = it.second;
+
+            if(mt.find(ch) == mt.end()) return false;
+            if(mt[ch] != freq) return false;
+        }
+        return true;
+    }
+};
