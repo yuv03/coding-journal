daeb98231c8d9ef904365796f4b87fd2765044b6
Yuvraj Dholia <100825799+yuv03@users.noreply.github.com>
2026-08-02 23:53:05 +0530

    Time: 22 ms (40.19%), Space: 23.8 MB (30.62%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,3 +1,5 @@
+
+
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
@@ -31,17 +33,3 @@ public:
     }
 };
 
-// "aacabb"   "bbcbaa"
-// a=3           a=2
-// b=2           b=3
-// c=1           c=1
-// freq1 = [3,2,1, , , , ]
-//          0 1 2 3 4 5
-
-// freq2 = [2,3,1, , , , ]
-//          0 1 2 3 4 5
-
-// "cabbba"   "abbccc"
-// a=2           a=1
-// b=3           b=2
-// c=1           c=3
