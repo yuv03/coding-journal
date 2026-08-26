7cb7002b4deab8bbc62068b39735a064305bb000
Yuvraj Dholia <100825799+yuv03@users.noreply.github.com>
2026-08-27 00:27:51 +0530

    Time: 0 ms (100%), Space: 15.2 MB (95.48%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.cpp
@@ -6,30 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
-public:
-
-    void getLeaf(TreeNode* root, vector<int>& leafNodes){
-        if(root==NULL) return;
-
-        if(root->left ==NULL && root->right==NULL){
-            leafNodes.push_back(root->val);
+private:
+    void leafNodes(TreeNode* root, vector<int> & leafVec){
+        if(root== NULL) return ;
+        if(root->left== NULL && root->right==NULL){
+            leafVec.push_back(root->val);
         }
-        getLeaf(root->left, leafNodes);
-        getLeaf(root->right, leafNodes);
+
+        leafNodes(root->left, leafVec);
+        leafNodes(root->right, leafVec);
     }
-
-
+public:
     bool leafSimilar(TreeNode* root1, TreeNode* root2) {
         vector<int> v1;
         vector<int> v2;
 
-        getLeaf(root1, v1);
-        getLeaf(root2, v2);
-        if(v1==v2) return true;
+        leafNodes(root1, v1);
+        leafNodes(root2, v2);
+
+        if(v1==v2)return true;
         return false;
     }
 };
